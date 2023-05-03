2dacbc6a0b37013a6e23338177e1945f06759e63
HugoLDev <hugolope30@icloud.com>
2023-05-03 11:06:43 -0300

    AddView

--- Singn In/Extensions/String.swift
+++ Singn In/Extensions/String.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension String {
+    
+    func isValidEmail() -> Bool {
+        
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+
+        
+        return regex.firstMatch(in: self, range: NSRange(location: 0, length: count)) != nil
+    }
+}
+let passwordRegex = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])(?=.*[A-Z]).{6,}$")
--- Singn In/View/LoginView.swift
+++ Singn In/View/LoginView.swift
@@ -5,4 +5,100 @@
 //  Created by Hugo Lopes Mendonca on 03/05/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    var body: some View {
+        ZStack {
+            Color.white
+                 .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("Welcome Back!")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                        .padding(.top, 50)
+                }
+                Spacer()
+                
+                HStack {
+                 Image(systemName: "mail")
+                    TextField("Email", text: $email)
+                    
+                    Spacer()
+                    
+                    if(email.count != 0) {
+                        Image(systemName: email.isValidEmail() ? "checkmark" : "xmark")
+                            .fontWeight(.bold)
+                            .foregroundColor(email.isValidEmail() ? .green : .red)
+                    }
+                    
+                }
+                .padding()
+                .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.black)
+                )
+                .padding()
+                
+                
+                HStack {
+                 Image(systemName: "lock")
+                    TextField("Password", text: $password)
+                    
+                    Spacer()
+                    
+                    if(password.count != 0) {
+                        Image(systemName: "checkmark")
+                            .fontWeight(.bold)
+                            .foregroundColor(.green)
+                    }
+                }
+                .padding()
+                .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.black)
+                )
+                .padding()
+                
+                Button(action:{}) {
+                    Text("Don't have an account?")
+                        .foregroundColor(.black.opacity(0.7))
+                }
+                Spacer()
+                Spacer()
+               
+                Button {
+                    
+                } label: {
+                    Text("Sing In")
+                        .foregroundColor(Color.white)
+                        .font(.title3)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.black)
+                        )
+                        .padding()
+                }
+            }
+        }
+    }
+}
+
+
+
+
+struct LoginView_Previews: PreviewProvider{
+    static var previews: some View {
+        LoginView()
+    }
+}
